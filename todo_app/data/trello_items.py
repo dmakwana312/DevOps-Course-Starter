16c30f7fd8d201227a6417139f6b46218bbd0087
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2023-09-18 16:40:16 +0100

    Comments added to trello_items.py

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -10,6 +10,13 @@ def build_params(additional_params = {}):
     return default_params
 
 def get_items():
+    """
+    Fetches all saved items from the Trello board.
+
+    Returns:
+        list: The list of saved items.
+    """
+
     url = build_url('boards/' + config.BOARD_ID + '/lists?cards=open')
     response = requests.get(url, params=build_params())
     response_json = response.json()
@@ -24,11 +31,25 @@ def get_items():
     return items
 
 def get_lists():
+    """
+    Fetches all lists from the Trello board.
+
+    Returns:
+        list: The list of Trello lists
+    """
+
     url = build_url('boards/' + config.BOARD_ID + '/lists')
     response = requests.get(url, params=build_params())
     return response.json()
 
 def get_list_id(name):
+    """
+    Fetches the id of the list.
+
+    Returns:
+        string: The id of the list requested
+    """
+
     all_lists = get_lists()
 
     for list in all_lists:
@@ -36,26 +57,53 @@ def get_list_id(name):
             return list['id']
 
     return None
-    
 
 def mark_complete(id):
+    """
+    Moves an item in the 'To Do' list to the 'Done' list
+
+    Returns:
+        boolean: True or False based on outcome of API call
+    """
+
     done_list_id = get_list_id(config.DONE_LIST_NAME)
     url = build_url('cards/' + id)
     response = requests.put(url, params=build_params({ 'idList':  done_list_id }))
     return response.status_code == 200
 
 def mark_incomplete(id):
+    """
+    Moves an item in the 'Done' list to the 'To Do' list
+
+    Returns:
+        boolean: True or False based on outcome of API call
+    """
+
     to_do_list_id = get_list_id(config.TO_DO_LIST_NAME)
     url = build_url('cards/' + id)
     response = requests.put(url, params=build_params({ 'idList':  to_do_list_id }))
     return response.status_code == 200
 
 def delete_item(id):
+    """
+    Deletes an item
+
+    Returns:
+        boolean: True or False based on outcome of API call
+    """
+
     url = build_url('cards/' + id)
     response = requests.delete(url, params=build_params())
     return response.status_code == 200
 
 def add_item(name):
+    """
+    Creates an item
+
+    Returns:
+        boolean: True or False based on outcome of API call
+    """
+
     to_do_list_id = get_list_id(config.TO_DO_LIST_NAME)
     url = build_url('cards/')
     response = requests.post(url, params=build_params({ 'idList':  to_do_list_id, 'name': name }))
