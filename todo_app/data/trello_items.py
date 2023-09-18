82578645ad49569d5c418a96999241e4c260af4d
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2023-09-18 16:24:24 +0100

    API config setup to use environment variables

--- todo_app/data/trello_items.py
+++ todo_app/data/trello_items.py
@@ -1,12 +1,16 @@
 import requests
+import todo_app.data.trello_config as config
+
+def build_url(endpoint):
+    return config.BASE_URL + endpoint
 
 def build_params(additional_params = {}):
-    default_params = { 'key': '062e02a471e570c709c77f10f1249e0f', 'token': 'ATTA8ac5a77f612d898fbb18d09b18fe237fabe01e6a306d151a773307df920dbd42624E5C0F' }
+    default_params = { 'key': config.API_KEY, 'token': config.API_TOKEN }
     default_params.update(additional_params)
     return default_params
 
 def get_items():
-    url = 'https://api.trello.com/1/boards/64f5c617d9a26e50962586f6/lists?cards=open'
+    url = build_url('boards/' + config.BOARD_ID + '/lists?cards=open')
     response = requests.get(url, params=build_params())
     response_json = response.json()
 
@@ -15,12 +19,12 @@ def get_items():
     for list in response_json:
         if list['cards'] != []:
             for card in list['cards']:
-                items.append( { 'id': card['id'], 'status': 'Complete' if list['name'] == 'Done' else "Not Started" , 'title': card['name'] })
+                items.append( { 'id': card['id'], 'status': 'Complete' if list['name'] == config.DONE_LIST_NAME else "Not Started" , 'title': card['name'] })
 
     return items
 
 def get_lists():
-    url = 'https://api.trello.com/1/boards/64f5c617d9a26e50962586f6/lists'
+    url = build_url('boards/' + config.BOARD_ID + '/lists')
     response = requests.get(url, params=build_params())
     return response.json()
 
@@ -35,25 +39,25 @@ def get_list_id(name):
     
 
 def mark_complete(id):
-    done_list_id = get_list_id('Done')
-    url = 'https://api.trello.com/1/cards/' + id
+    done_list_id = get_list_id(config.DONE_LIST_NAME)
+    url = build_url('cards/' + id)
     response = requests.put(url, params=build_params({ 'idList':  done_list_id }))
     return response.status_code == 200
 
 def mark_incomplete(id):
-    to_do_list_id = get_list_id('To Do')
-    url = 'https://api.trello.com/1/cards/' + id
+    to_do_list_id = get_list_id(config.TO_DO_LIST_NAME)
+    url = build_url('cards/' + id)
     response = requests.put(url, params=build_params({ 'idList':  to_do_list_id }))
     return response.status_code == 200
 
 def delete_item(id):
-    url = 'https://api.trello.com/1/cards/' + id
+    url = build_url('cards/' + id)
     response = requests.delete(url, params=build_params())
     return response.status_code == 200
 
 def add_item(name):
-    to_do_list_id = get_list_id('To Do')
-    url = 'https://api.trello.com/1/cards/'
+    to_do_list_id = get_list_id(config.TO_DO_LIST_NAME)
+    url = build_url('cards/')
     response = requests.post(url, params=build_params({ 'idList':  to_do_list_id, 'name': name }))
     return response.status_code == 200
 
