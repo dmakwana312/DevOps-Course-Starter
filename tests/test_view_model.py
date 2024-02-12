bfec168823845b05b99d1a2a54fbf524e2abfb89
Dipesh Makwana <dipesh_makwana@hotmail.co.uk>
2024-02-12 14:48:31 +0000

    Changes made based on exercise-5 PR

--- tests/test_view_model.py
+++ tests/test_view_model.py
@@ -16,16 +16,19 @@ def test_done_items_when_no_done_item():
     assert len(view_model.done_items) == 0
 
 def test_done_items_when_done_item_exists():
+
+    done_item=Item(2, 'Task 2', datetime.now(), DONE_LIST_NAME)
+
     items = [
         Item(1, 'Task 1', datetime.now(), TO_DO_LIST_NAME),
-        Item(2, 'Task 2', datetime.now(), DONE_LIST_NAME)
+        done_item
     ]
 
     view_model = ViewModel(items)
     
     assert len(view_model.items) == 2
     assert len(view_model.done_items) == 1
-    assert all(view_model.done_items) == all([Item(2, 'Task 2', datetime.now(), DONE_LIST_NAME)])
+    assert view_model.done_items == [done_item]
 
 def test_todo_items_when_no_todo_item_exist():
     items = [
